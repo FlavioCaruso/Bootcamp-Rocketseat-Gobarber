4384700cfc069468c4f926275504f261d6c95c5d
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-10-15 15:56:24 -0300

    gerado o hash da senha

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Flávio Caruso',
-    email: 'fbcaruso@hotmail.com',
-    password_hash: '12345',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
