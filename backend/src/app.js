e2b974ab1cf9b067a319d181494ab05c646d5848
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-10-15 14:34:10 -0300

    ajustes no ESLint, Prettier e EditorConfig

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+// Para utilizar o import e export deve instalar a dependencia "sucrase" e precisa rodar por ele
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
