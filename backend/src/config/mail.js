3cb5aa611f7de73b57eac436198e70175e2c83fe
Flavio <fbcaruso@hotmail.com>
2019-10-18 21:14:39 -0300

    Nodemailer Configurado

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '808fdd1c023815',
+    pass: '8429bfadf96919',
+  },
+  default: {
+    from: 'Flavio Caruso <noreply@gobarber.com>',
+  },
+};
