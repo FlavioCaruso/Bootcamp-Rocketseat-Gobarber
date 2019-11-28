35d7f56f778a6f8bf559f974b2b65fa3fb20d880
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-11-28 09:17:38 -0300

    Criado a estilização da autenticação

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Seu-email" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
   );
 }
