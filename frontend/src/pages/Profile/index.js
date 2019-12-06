4bcae2fae8c06076cad6c5700a3d245cea9302ab
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-12-06 13:35:53 -0300

    Ajustes na página de perfil

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu endereço de Email" />
+        <hr />
+        <input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <input type="password" name="password" placeholder="Nova senha" />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
